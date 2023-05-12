9886e87fe4957dd839b48715bf05de33098fa4e1
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2023-05-12 06:01:50 +0500

    update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.up.railway.app', '.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
