a8399680668e0d9b99694f623f3629435a5c020e
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2023-05-21 21:29:48 +0500

    update urls.py

--- negPosAPI/urls.py
+++ negPosAPI/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from negPosAPI.views import CommentViewSet
 
 urlpatterns = [
-    path('articles/<int:article_id>/comments', CommentViewSet.as_view({'post': 'create'})),
     path('articles/<int:article_id>/comments/', CommentViewSet.as_view({'post': 'create'})),
 ]
